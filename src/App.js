c3e8a506ed1dbc4b99775d3817f756cd08db0004
Tamires <tamiressiqueira12@gmail.com>
2024-10-13 14:25:24 -0300

    iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'; 
 
 function App() {
+
+  const [products,setProducts]=useState([]);
+
+  useEffect(()=>{
+async function callProducts() {
+  const response= await fetch('https://backend-production-3a10.up.railway.app/produtos');
+  const data= await response.json();
+  console.log(data);
+  setProducts(data);
+}
+callProducts();
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+<ul> {products.map(product=>
+  <li >{product.nome}</li>
+)}</ul>
       </header>
     </div>
   );
